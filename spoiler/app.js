d035a5770a0fe20f1accaaafe913d8e094687646
Slumrag <mrgreenthecristal@yandex.ru>
2023-10-10 22:01:42 +0300

    feat: add toggle logic to spoiler

--- spoiler/app.js
+++ spoiler/app.js
@@ -1,1 +1,33 @@
-const myButton = document.getElementById('myBtn');
+'use strict';
+const toggleSpoiler = getSpoilerToggleCallback('#spoiler', '#myBtn');
+document.addEventListener('click', toggleSpoiler);
+document.addEventListener('keyup', toggleSpoiler);
+
+function getSpoilerToggleCallback(spoilerSelector, buttonSelector) {
+  const spoiler = document.querySelector(spoilerSelector);
+  let isClosed = spoiler?.classList?.contains('closed');
+  const hideSpoiler = () => {
+    spoiler?.classList?.add('closed');
+    isClosed = true;
+  };
+  const showSpoiler = () => {
+    spoiler?.classList?.remove('closed');
+    isClosed = false;
+  };
+  const toggleSpoiler = () => {
+    if (isClosed) {
+      showSpoiler();
+    } else {
+      hideSpoiler();
+    }
+  };
+  return (event) => {
+    if (event?.type === 'click' && event?.target?.closest(buttonSelector)) {
+      toggleSpoiler();
+    }
+
+    if (event?.type === 'keyup' && event?.code === 'Escape' && !isClosed) {
+      hideSpoiler();
+    }
+  };
+}
