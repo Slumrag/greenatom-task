5961860570cb79be74f3473ff593ba19d0cd3618
Slumrag <mrgreenthecristal@yandex.ru>
2023-12-23 17:46:29 +0300

    feat: configure resources

--- webpack-task/src/App.tsx
+++ webpack-task/src/App.tsx
@@ -1,9 +1,15 @@
 import React, { FC } from 'react';
-
+import './styles.css';
+import pngLogo from './images/logo.png';
+import svgLogo from './images/react-2.svg';
 const App: FC = () => {
   return (
     <div>
       <h1>app</h1>
+      <div className=''>
+        <img src={pngLogo} alt='png' width={70} />
+        <img src={svgLogo} alt='svg' width={70} />
+      </div>
     </div>
   );
 };
