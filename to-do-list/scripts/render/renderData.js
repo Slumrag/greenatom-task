90161710c88c00a6eac24ebc60a04c461bdbef28
Slumrag <mrgreenthecristal@yandex.ru>
2023-12-31 15:55:39 +0300

    Updates

--- to-do-list/scripts/render/renderData.js
+++
@@ -1,8 +0,0 @@
-import { renderToDos } from './renderToDos.js';
-import { renderUsers } from './renderUsers.js';
-
-export function renderData({ users, toDos }, { optionsContainer, listContainer }) {
-  // console.table(toDos);
-  renderUsers(users, optionsContainer);
-  renderToDos(toDos, listContainer);
-}
--- to-do-list/scripts/utils/groupArrayBy.js
+++
@@ -1,8 +0,0 @@
-export function groupArrayBy(arr, field) {
-  return arr.reduce((acc, curr) => {
-    const newKey = curr[field];
-    delete curr[field];
-    // console.log(arr, curr, _);
-    return { ...acc, [newKey]: curr };
-  }, {});
-}
